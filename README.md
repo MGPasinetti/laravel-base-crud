653c8a1c13db8f668d9609ee6855112d68ac7c17
MGPasinetti <mg.pasinetti@gmail.com>
2022-05-13 15:40:17 +0200

    working on edit function and view

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Iniziate a definire le operazioni CRUD per gestire un archivio di fumetti.
 BONUS: creare il seeder per la tabella comics utilizzando il file in allegato.
 
 Ciao ragazzi, oggi lavorate sulla stessa repo di lunedì (laravel-base-crud) e aggiungete l'azione di creazione di una nuova risorsa al sito (la C in CRUD: create). Nel frattempo continuate anche a lavorare al front-end del sito.
+
+Ciao ragazzi, oggi lavorate sulla stessa repo laravel-base-crud e aggiungete l'azione di update e di delete delle risorse (la U e la D in CRUD), implementando anche la validazione dei dati.
---
+++ resources/views/comics/edit.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $comic->title }}</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div class="container">
+        <div class="row g-4">
+            <div class="col-8">
+                <h2>{{ $comic->title }}</h2>
+                <h5>{{ $comic->series }}</h5>
+                <p>{{ $comic->description }}</p>
+                <p>TYPE: {{ $comic->type }}</p>
+                <h4>{{ $comic->price }}£</h4>
+                <p>SALE DATE: {{ $comic->sale_date }}</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+
+                <form method="POST" action="{{ route('comics.update', $comic->id) }}">
+                    @csrf
+                    @method('PUT')
+
+                    <div class="mb-3">
+                        <label for="thumb" class="form-label">Thumb</label>
+                        <input type="text" class="form-control" id="thumb" name="thumb">
+                      </div>
+                    <div class="mb-3">
+                      <label for="title" class="form-label">Title</label>
+                      <input type="text" class="form-control" id="title" name="title">
+                    </div>
+                    <div class="mb-3">
+                        <label for="description" class="form-label">Description</label>
+                        <input type="text" class="form-control" id="description" name="description">
+                    </div>
+                    <div class="mb-3">
+                        <label for="price" class="form-label">Price</label>
+                        <input type="text" class="form-control" id="price" name="price">
+                    </div>
+                    <div class="mb-3">
+                        <label for="series" class="form-label">Series</label>
+                        <input type="text" class="form-control" id="series" name="series">
+                    </div>
+                    <div class="mb-3">
+                        <label for="sale_date" class="form-label">Sale Date</label>
+                        <input type="date" class="form-control" id="sale_date" name="sale_date">
+                    </div>
+                    <div class="mb-3">
+                        <label for="type" class="form-label">Type</label>
+                        <input type="text" class="form-control" id="type" name="type">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+                <a href="{{ url()->previous() }}">Back</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
