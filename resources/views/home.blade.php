81dc1d11b98eb2f9a6e6ee9115505866a87df3a4
MGPasinetti <mg.pasinetti@gmail.com>
2022-05-16 18:33:20 +0200

    worked on cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,17 +5,17 @@
 @section('pageMain')
 
 <div class="container p-5">
-        <div class="row row-cols-3 row-cols-lg-4 g-4">
+        <div class="row row-cols-3 g-4">
             @foreach ($comics as $comic)
                 <div class="col d-flex align-items-stretch">
                     <div class="card">
                         <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}">
                         <div class="card-body">
                           <h3 class="card-title mb-4">{{ $comic->title }}</h3>
-                          <p class="card-text overflow-scroll" style="height: 15rem">{{ $comic->description }}</p>
-                          <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-primary">See more</a>
+                          <p class="card-text overflow-scroll mb-5" style="max-height: 250px">{{ $comic->description }}</p>
+                          <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-primary" style="position: absolute; left: 1rem; bottom: 1rem">See more</a>
                         </div>
-                      </div>
+                    </div>
                 </div>
             @endforeach
         </div>
