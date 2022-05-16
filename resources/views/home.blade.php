bb3bae1c97e3920bde6253140bb1215cf1732107
MGPasinetti <mg.pasinetti@gmail.com>
2022-05-16 17:55:38 +0200

    working on home grid-cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,18 @@
 
 @section('pageMain')
 
-<div class="container">
-        <div class="row g-4">
+<div class="container p-5">
+        <div class="row row-cols-3 row-cols-lg-4 g-4">
             @foreach ($comics as $comic)
-                <div class="col-4">
-                    <div class="card h-100">
-                        <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+                <div class="col d-flex align-items-stretch">
+                    <div class="card">
+                        <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}">
                         <div class="card-body">
-                            <h2 class="card-title mb-4"><a href="{{ route('comics.show', $comic->id) }}">{{ $comic->title }}</a></h2>
-                            <p class="card-text overflow-scroll" style="height: 15rem"><strong>DESCRIPTION:</strong><br> {{ $comic->description }}</p>
+                          <h3 class="card-title mb-4">{{ $comic->title }}</h3>
+                          <p class="card-text overflow-scroll" style="height: 15rem">{{ $comic->description }}</p>
+                          <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-primary">See more</a>
                         </div>
-                    </div>
+                      </div>
                 </div>
             @endforeach
         </div>
